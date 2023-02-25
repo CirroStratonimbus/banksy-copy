e8b630cef70dda29bd3573d3fc008f422084346e
CirroStratonimbus <60374855+CirroStratonimbus@users.noreply.github.com>
2023-02-25 13:01:05 +0000

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <NavigationBar/>
       <Routes>
-        <Route exact path='/' element={<Outside/>}/>
+        <Route exact path='banksy-copy/' element={<Outside/>}/>
         <Route path='/outside' element={<Outside/>}/>
         <Route path='/inside' element={<Inside/>}/>
         <Route path='/q+a' element={<QA props={QAimg}/>}/>
